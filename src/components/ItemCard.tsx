d42c65979a905c427d1747cdc2ab49788884beb7
sai-sukrutha <csaisukrutha@gmail.com>
2024-12-16 16:37:24 -0600

    Added Experience page - using Card Comp + Added Icons

---
+++ src/components/ItemCard.tsx
@@ -0,0 +1,30 @@
+import './styles/ItemCard.scss';
+
+import {PiMapPin, PiCalendarBlank  } from 'react-icons/pi'
+
+interface ItemCardProps {
+    img : string;       // TODO: Is image compulsary
+    title: string;
+    role: string;
+    title_link: string;        // Link to institution/company in title & github for projects
+    location?: string;          // Below all these for companies and education only
+    start_date?: string;
+    end_date?: string;
+    description?: string;
+}
+
+export const ItemCard = (props: ItemCardProps) => {
+  return (
+    <div className='card'>
+        <img className='card_img' src={props.img}/>      {/* TODO: href to where for image and title or anywhere - to description, there to website??  */}
+        <div className='card_body' >
+            <h2 className='card_title'><a href={props.title_link} target='_blank'>{props.title}</a></h2>
+            <p className='card_role'>{props.role}</p>
+            <p className='dates'> <PiCalendarBlank/> {props.start_date} - {props.end_date}</p>
+            <p className='location'> <PiMapPin/> {props.location}</p>
+            <p className='description'>{props.description}</p>  {/*How to show description?? */}
+        </div>
+    </div>
+
+  )
+}
--- src/components/pages/Experience.tsx
+++ src/components/pages/Experience.tsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import { ItemCard } from '../ItemCard';
 
 const Experience = () => {
     return (
-        <div>Experience</div>
+        <div className='wrapper' >
+            <ItemCard
+                img = 'images/mirelz.jpeg'
+                title = 'Mirelz AI Pvt Limited'
+                title_link='https://mirelz.com/'
+                role='Software Engineer, Full Stack Developer'
+                location='Hyderabad, India'
+                start_date='June 2020'
+                end_date = 'June 2023'
+            />
+            <ItemCard
+                img = 'images/adobe.png'
+                title = 'Adobe'
+                title_link='https://www.adobe.com/'
+                role='Summer Product Intern'
+                location='Banglore, India'
+                start_date='May 2019'
+                end_date = 'July 2019'
+            />
+            <ItemCard
+                img = 'images/tcs.png'
+                title = 'Tata Consultancy Services'
+                title_link='https://www.tcs.com/'
+                role='Systems Engineer'
+                location='Hyderabad, India'
+                start_date='August 2016'
+                end_date = 'July 2018'
+            />
+        </div>
+
+
+        
+
     );
 }
 
