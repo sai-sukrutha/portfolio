262e42a86a6e9c4e8f6861ef81a046149422d2a3
sai-sukrutha <csaisukrutha@gmail.com>
2025-01-02 22:05:39 -0600

    Getting Data from JSON-bug fix

--- src/components/ProjectComponents.tsx
+++ src/components/ProjectComponents.tsx
@@ -14,8 +14,8 @@ export const ProjectCard = (props: ProjectProps) => {
     <div className='card'>
         <img className='card_img' src={props.img} alt={altText}/>      {/* TODO: href to where for image and title or anywhere - to description, there to website??  */}
         <div className='card_body' >
-            {/* <h2 className='card_title'><a href={props.titleLink} target='_blank'>{props.name}</a></h2> */}
-            <h2 className='card_title'><Link key={props.name} to={`/experiences/${props.id}`}>{props.name}</Link></h2>
+            <h2 className='card_title'><a href={props.titleLink} target='_blank'>{props.name}</a></h2>
+            {/* <h2 className='card_title'><Link key={props.name} to={`/experiences/${props.id}`}>{props.name}</Link></h2> */}
             <p className='card_role'>{props.role}</p>
             <p className='dates'> <PiCalendarBlank/> {props.startDate} - {props.endDate}</p>
             <p className='location'> <PiMapPin/> {props.location}</p>
